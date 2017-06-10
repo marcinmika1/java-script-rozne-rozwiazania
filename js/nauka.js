b43a77391c29c3135b355ab52b531db3cd7c347c
Marcin Mika <marcinmika@hotmail.com>
2017-06-10 10:50:33 +0200

    uzycie false i true

--- js/nauka.js
+++ js/nauka.js
@@ -18,10 +18,55 @@ var wyplataStr = "5000";
 var premiaStr = "2000";
 var calaWyplata = wyplataStr + "\n \" dodajemy\" \n " + premiaStr;
 
+//znaki specjalne maja na poczatku "\" np \n - nowa linia 
+
 /*wyswietlenie wyniku */
 
 console.log(calaWyplata);
 
 //*************************************************************
+
+/*metody w JS */
+
+var imieStudenta='     Michał';
+
+console.log(imieStudenta.indexOf('c'));
+/* na ktorej pozycji jest literka c liczymy od 0*/
+console.log(imieStudenta);
+console.log(imieStudenta.trim());
+/* usuwanie spacji na poczatku i na koncu*/
+
+//****************************************************************
+
+/*Użycie true i false */
+
+var czyJestSmok;
+czyJestSmok = true;
+
+if (czyJestSmok) 
+    {
+        console.log('jest smog') /*wyswietl jest smog jesli prawda*/
+    }
+else
+    {
+        console.log('nie ma smogu') /*wyswietl nie ma smog jesli nie prawda*/
+    }
+
+//*******************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
