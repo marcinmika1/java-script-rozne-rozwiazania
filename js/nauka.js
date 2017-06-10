ab3458975d3c8bce7300b639fc14eb342d61fe2d
Marcin Mika <marcinmika@hotmail.com>
2017-06-10 10:25:57 +0200

    działania na zmiennych string

--- js/nauka.js
+++ js/nauka.js
@@ -1,7 +1,27 @@
 'use strict';
+//**********************************************************
+/*deklaracja zmiennych i nadawanie wartosci zeminnym*/
 
 var wyplata = 5000;
 var premia = 2000;
 var calkowitaWyplata = wyplata + premia;
 
+/*wyswietlenie wyniku*/
+
 console.log(calkowitaWyplata);
+
+//*********************************************************
+
+/*działania na stringach*/
+
+var wyplataStr = "5000";
+var premiaStr = "2000";
+var wyplata = wyplataStr + "\n" + premiaStr;
+
+/*wyswietlenie wyniku */
+
+console.log(wyplata);
+
+//*************************************************************
+
+
