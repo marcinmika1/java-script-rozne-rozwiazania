794ecb02bbac60b84227ff71be359fbe3456d6eb
Marcin Mika <marcinmika@hotmail.com>
2017-06-10 17:01:03 +0200

    JSON

--- js/nauka.js
+++ js/nauka.js
@@ -354,12 +354,49 @@ var marek = new nazwaFunkcji1 ('marek', 'nowak');
 
 
 console.log(nazwaFunkcji1);
-***********************************************
+//***********************************************
 /*JSON*/
 
+var jsonOsoby = {
+     "osoby": [
+         {
+             imie: "Krystian",
+             nazwisko: "Dziopa",
+             wzrost: 180,
+             oczy: "niebieskie",
+             zainteresowania: [
+                 { nazwa: "podroze" },
+                 { nazwa: "gotowanie" }
+             ]
+         },
+         {
+             imie: "Dominik",
+             nazwisko: "Gomuła",
+             wzrost: 170,
+             oczy: "brązowe",
+             zainteresowania: [
+                 { nazwa: "gotowanie" },
+                 { nazwa: "języki" }
+             ]
+         },
+         {
+             imie: "Claudia",
+             nazwisko: "Żelazowska",
+             wzrost: 167,
+             oczy: "brązowe",
+             zainteresowania: [
+                 { nazwa: "programowanie" },
+                 { nazwa: "podroze" },
+                 { nazwa: "języki" }
+             ]
 
+         }
+     ]
+}
 
+console.log(jsonOsoby);
 
+jsonOsoby.osoby[2].zainteresowania.forEach(function (element,index){console.log(element.nazwa);})
 
 
 
